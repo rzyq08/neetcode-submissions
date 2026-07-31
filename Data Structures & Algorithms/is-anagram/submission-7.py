c69b887bbc9121c1fa0dbb242ea57981db8679ce
Arfa Raziq <155065572+rzyq08@users.noreply.github.com>
2026-08-01 00:43:33 +0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        alph = [0]*26
+        for i in range(len(s)):
+            alph[ord(s[i])-ord('a')]+=1
+            alph[ord(t[i])-ord('a')]-=1
+        
+        for num in alph:
+            if num!=0:
+                return False
+        return True
